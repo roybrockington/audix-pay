d25f0d95660d40633541fe9f0995bcb3e53ce7bd
roybrockington <r.brockington@gmail.com>
2024-09-05 12:15:25 +0100

    Test data map

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.scvdistribution.co.uk',
+      },
+    ],
+  },
+
+}
+
 
 export default nextConfig;
